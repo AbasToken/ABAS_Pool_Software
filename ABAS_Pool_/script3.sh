b3495d603be2573247d8debd967187abf11694cd
ABAS Token <120234697+AbasToken@users.noreply.github.com>
2022-12-21 14:38:21 -0600

    Delete script3.sh

--- ABAS_Pool_/script3.sh
+++
@@ -1,7 +0,0 @@
- export BASE_ACCOUNT='0xdFD18d5374dF275A8EDE107f981f78b1E60f63E4'
- export PRIVATE_KEY="cb0be61d9500fe06a8b8ac607cea950523aeab39c04dfd7bbaacb933778205c8"
- export ETH_PROVIDER_URL="https://polygon-rpc.com/"
-cd python-distribution/
-python3 Addstats.py
-
-
